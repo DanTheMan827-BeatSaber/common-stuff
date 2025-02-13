1f7848b61bcb3f6a29781df0825c393880095827
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-02-13 17:12:33 -0600

    Delete Logger.hpp, modInfo.hpp, and 00-make-hooking.cmake

--- cmake/00-make-hooking.cmake
+++
@@ -1,6 +0,0 @@
-include_guard()
-
-execute_process(
-    COMMAND pwsh make-hooking.ps1
-    WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/../scripts"
-)
--- include/Logger.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "paper2_scotland2/shared/logger.hpp"
-
-/// @brief A logger, useful for printing debug messages
-/// @return
-inline constexpr auto Logger = Paper::ConstLoggerContext(MOD_ID "_" VERSION);
