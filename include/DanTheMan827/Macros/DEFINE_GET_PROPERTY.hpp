64bb1dccca54889c426f6c9ab4e6cb8b22e03458
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-05-19 22:03:55 -0500

    refactor: remove DEFINE_GET_PROPERTY macro
    - Deleted the DEFINE_GET_PROPERTY.hpp file

--- include/DanTheMan827/Macros/DEFINE_GET_PROPERTY.hpp
+++
@@ -1,9 +0,0 @@
-/// @brief Macro to define a getter property for a static class member variable
-#define DEFINE_GET_PROPERTY(name, type)                                  \
-   public:                                                               \
-    static inline __declspec(property(get = __GETTER_##name)) type name; \
-                                                                         \
-   private:                                                              \
-    template <typename T = void>                                         \
-    requires(std::is_void<T>::value)                                     \
-    static inline type __GETTER_##name()
